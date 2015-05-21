d3022611cb52ed2ed11a1edf82addd8047de35f3
Rui Campos <rui.campos@gameduell.de>
2015-05-21 16:29:40 +0200

    added licenses to everything in preparation for open source

--- runloop/RunLoop.hx
+++ runloop/RunLoop.hx
@@ -1,8 +1,27 @@
 /*
- * Created by IntelliJ IDEA.
- * User: rcam
- * Date: 12/06/14
- * Time: 16:09
+ * Copyright (c) 2003-2015, GameDuell GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package runloop;
@@ -290,7 +309,7 @@ class RunLoop
     }
 
     /// run every loop
-    public function addLoopObserver(func: RunLoop->Void): Void 
+    public function addLoopObserver(func: RunLoop->Void): Void
     {
         loopObservers.push(func);
     }
@@ -306,7 +325,7 @@ class RunLoop
             }
         }
     }
-    
+
     public function queue(func : Void->Void, priority : Priority) : Void
     {
 
